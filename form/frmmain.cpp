694cae6068dfeb722dc1fdf4d2db219894f3f632
liuzhanchun <liuzhanchun>
2019-10-15 16:49:52 +0800

    init v1.0

---
+++ form/frmmain.cpp
@@ -0,0 +1,15 @@
+﻿#include "frmmain.h"
+#include "ui_frmmain.h"
+
+frmMain::frmMain(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::frmMain)
+{
+    ui->setupUi(this);
+    ui->tabWidget->setCurrentIndex(0);
+}
+
+frmMain::~frmMain()
+{
+    delete ui;
+}
